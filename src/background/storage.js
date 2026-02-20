978dfac823dc82db8ed0e61e70cb406c3049252a
Y.S. <fagionpw@gmail.com>
2026-02-21 03:29:57 +0700

    Create safe chrome.storage wrapper

---
+++ src/background/storage.js
@@ -0,0 +1,41 @@
+// Safe Chrome Storage Wrapper
+
+const storage = {
+    setItem: (key, value) => {
+        return new Promise((resolve, reject) => {
+            chrome.storage.local.set({ [key]: value }, () => {
+                if (chrome.runtime.lastError) {
+                    reject(chrome.runtime.lastError);
+                } else {
+                    resolve();
+                }
+            });
+        });
+    },
+
+    getItem: (key) => {
+        return new Promise((resolve, reject) => {
+            chrome.storage.local.get([key], (result) => {
+                if (chrome.runtime.lastError) {
+                    reject(chrome.runtime.lastError);
+                } else {
+                    resolve(result[key]);
+                }
+            });
+        });
+    },
+
+    removeItem: (key) => {
+        return new Promise((resolve, reject) => {
+            chrome.storage.local.remove([key], () => {
+                if (chrome.runtime.lastError) {
+                    reject(chrome.runtime.lastError);
+                } else {
+                    resolve();
+                }
+            });
+        });
+    }
+};
+
+export default storage;
